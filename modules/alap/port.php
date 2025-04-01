ad441bd4ca7fd5827cc10c12cc69c5c3ad7e6872
sgtGiggsy <sgtgiggsy@gmail.com>
2025-04-01 23:02:27 +0200

    Port megjelenítése - újraírt query, a megjelenítés még nincs hozzá igazítva

--- modules/alap/port.php
+++ modules/alap/port.php
@@ -13,49 +13,83 @@ else
 	}
 	else
 	{
-		$portres = new MySQLHandler("SELECT portok.id AS portid,
-				portok.port AS port,
-				epuletek.nev AS epuletnev,
-				helyisegszam,
-				helyisegnev,
-				helyisegek.id AS helyisegid,
-				rackszekrenyek.nev AS rack,
-				rackszekrenyek.helyiseg AS rackhelyiseg,
-				(SELECT helyisegnev FROM helyisegek WHERE id = rackhelyiseg) as rackhelynev,
-				(SELECT helyisegszam FROM helyisegek WHERE id = rackhelyiseg) as rackhelyszam,
-				(SELECT port FROM portok WHERE csatlakozas = portid ORDER BY id ASC LIMIT 1) AS szomszedport,
-				(SELECT id FROM portok WHERE csatlakozas = portid LIMIT 1) AS szomszedportid,
-				(SELECT beepitesek.nev FROM switchportok
-						INNER JOIN eszkozok ON switchportok.eszkoz = eszkozok.id
-						INNER JOIN beepitesek ON beepitesek.eszkoz = eszkozok.id
-					WHERE switchportok.port = szomszedportid) AS switch,
-				(SELECT ipcimek.ipcim FROM switchportok
-						INNER JOIN eszkozok ON switchportok.eszkoz = eszkozok.id
-						INNER JOIN beepitesek ON beepitesek.eszkoz = eszkozok.id
-						INNER JOIN ipcimek ON beepitesek.ipcim = ipcimek.id
-					WHERE switchportok.port = szomszedportid) AS switchip,
-				(SELECT port FROM portok WHERE csatlakozas = portid ORDER BY id DESC LIMIT 1) AS szomszedport2,
-				(SELECT id FROM portok WHERE csatlakozas = portid ORDER BY id DESC LIMIT 1) AS szomszedportid2,
-				(SELECT beepitesek.nev FROM switchportok
-						INNER JOIN eszkozok ON switchportok.eszkoz = eszkozok.id
-						INNER JOIN beepitesek ON beepitesek.eszkoz = eszkozok.id
-					WHERE switchportok.port = szomszedportid2) AS switch2,
-				(SELECT ipcimek.ipcim FROM switchportok
-						INNER JOIN eszkozok ON switchportok.eszkoz = eszkozok.id
-						INNER JOIN beepitesek ON beepitesek.eszkoz = eszkozok.id
-						INNER JOIN ipcimek ON beepitesek.ipcim = ipcimek.id
-					WHERE switchportok.port = szomszedportid2) AS switchip2
-			FROM portok
-				LEFT JOIN switchportok ON portok.id = switchportok.port
-				LEFT JOIN vegpontiportok ON portok.id = vegpontiportok.port
-				LEFT JOIN rackportok ON portok.id = rackportok.port
-				LEFT JOIN tkozpontportok ON portok.id = tkozpontportok.port
-				LEFT JOIN rackszekrenyek ON rackportok.rack = rackszekrenyek.id
-				LEFT JOIN eszkozok ON switchportok.eszkoz = eszkozok.id
-				LEFT JOIN beepitesek ON eszkozok.id = beepitesek.eszkoz
-				LEFT JOIN helyisegek ON vegpontiportok.helyiseg = helyisegek.id
-				LEFT JOIN epuletek ON helyisegek.epulet = epuletek.id
-			WHERE portok.id = ?;", $portid);
+		$portres = new MySQLHandler("SELECT port_id, szomszedport_id, portkapcsolat_id,
+					portpar.port AS port,
+					portpar.csatlakozo AS csatlakozo,
+					epuletek.nev AS epuletnev,
+					helyisegek.helyisegszam AS helyisegszam,
+					helyisegek.helyisegnev AS helyisegnev,
+					helyisegek.id AS helyisegid,
+					rackszekrenyek.nev AS rack,
+					rackszekrenyek.helyiseg AS rackhelyiseg,
+					rackhely.helyisegszam AS rackhelyszam,
+					rackhely.helyisegnev AS rackhelynev,
+					rackepulet.nev AS rackepuletnev,
+					beepitesek.id AS beepid,
+					beepitesek.aktivbeepites AS aktivbeepites,
+					beepitesek.nev AS switch,
+					ipcimek.ipcim AS switchip,
+					IF(vegpontiportok.id, 1,
+					IF(transzportportok.id, 2,
+						IF(switchportok.id, 3,
+							IF(tkozpontportok.id, 4, 0)))) AS porttipus,
+							szomszed.port AS szomszport,
+					szomszep.nev AS szomszepuletnev,
+					szomszhelyiseg.helyisegszam AS szomszhelyisegszam,
+					szomszhelyiseg.helyisegnev AS szomszhelyisegnev,
+					szomszhelyiseg.id AS szomszhelyisegid,
+					rackszekrenyek.nev AS szomszrack,
+					szomszrack.helyiseg AS szomszrackhelyiseg,
+					szomszrackhely.helyisegszam AS szomszrackhelyszam,
+					szomszrackhely.helyisegnev AS szomszrackhelynev,
+					szomszrackepulet.nev AS szomszrackepuletnev,
+					szomszbeep.id AS szomszbeepid,
+					szomszbeep.aktivbeepites AS szomszaktivbeepites,
+					szomszbeep.nev AS szomszswitch,
+					szomszip.ipcim AS szomszswitchip
+				FROM
+					(SELECT portok.id AS port_id, port, csatlakozo,
+							IF(portok.id = port_kapcsolatok.port_1, port_kapcsolatok.port_2, port_kapcsolatok.port_1) AS szomszedport_id,
+							portkapcsolat_id
+						FROM portok
+							LEFT JOIN port_kapcsolatok ON portok.id = port_kapcsolatok.port_1
+						WHERE portkapcsolat_id IS NOT NULL
+					UNION
+						SELECT portok.id AS port_id, port, csatlakozo,
+							IF(portok.id = port_kapcsolatok.port_2, port_kapcsolatok.port_1, port_kapcsolatok.port_2) AS szomszedport_id,
+							portkapcsolat_id
+						FROM portok
+							LEFT JOIN port_kapcsolatok ON portok.id = port_kapcsolatok.port_2
+						WHERE portkapcsolat_id IS NOT NULL
+					) AS portpar
+					LEFT JOIN transzportportok ON portpar.port_id = transzportportok.port
+					LEFT JOIN switchportok ON portpar.port_id = switchportok.port
+					LEFT JOIN vegpontiportok ON portpar.port_id = vegpontiportok.port
+					LEFT JOIN rackportok ON portpar.port_id = rackportok.port
+					LEFT JOIN tkozpontportok ON portpar.port_id = tkozpontportok.port
+					LEFT JOIN helyisegek ON vegpontiportok.helyiseg = helyisegek.id
+					LEFT JOIN eszkozok ON switchportok.eszkoz = eszkozok.id
+					LEFT JOIN beepitesek ON eszkozok.id = beepitesek.eszkoz
+					LEFT JOIN ipcimek ON beepitesek.ipcim = ipcimek.id
+					LEFT JOIN epuletek ON helyisegek.epulet = epuletek.id
+					LEFT JOIN rackszekrenyek ON rackportok.rack = rackszekrenyek.id
+					LEFT JOIN helyisegek rackhely ON rackszekrenyek.helyiseg = rackhely.id
+					LEFT JOIN epuletek rackepulet ON rackhely.epulet = rackepulet.id
+					LEFT JOIN portok szomszed ON szomszed.id = portpar.szomszedport_id
+					LEFT JOIN transzportportok szomsztransz ON szomszed.id = szomsztransz.port
+					LEFT JOIN switchportok szomszswp ON szomszed.id = szomszswp.port
+					LEFT JOIN vegpontiportok szomszvegp ON szomszed.id = szomszvegp.port
+					LEFT JOIN rackportok szomszrackp ON szomszed.id = szomszrackp.port
+					LEFT JOIN tkozpontportok szomsztkport ON szomszed.id = szomsztkport.port
+					LEFT JOIN helyisegek szomszhelyiseg ON szomszvegp.helyiseg = szomszhelyiseg.id
+					LEFT JOIN eszkozok szomszeszk ON szomszswp.eszkoz = szomszeszk.id
+					LEFT JOIN beepitesek szomszbeep ON szomszeszk.id = szomszbeep.eszkoz
+					LEFT JOIN ipcimek szomszip ON szomszbeep.ipcim = szomszip.id
+					LEFT JOIN epuletek szomszep ON szomszhelyiseg.epulet = szomszep.id
+					LEFT JOIN rackszekrenyek szomszrack ON szomszrackp.rack = szomszrack.id
+					LEFT JOIN helyisegek szomszrackhely ON szomszrack.helyiseg = szomszrackhely.id
+					LEFT JOIN epuletek szomszrackepulet ON szomszrackhely.epulet = szomszrackepulet.id
+			WHERE port_id = ?;", $portid);
 		$port = $portres->Fetch();
 		$port2 = $portres->Fetch();
 
